b16405aa5ef6c7ee9e7bec674b653041e8a3f4c1
spark5555 <pahalageumesh@gmail.com>
2026-08-01 21:07:50 +0000

    Playwright CI pipeline test

--- playwright.config.js
+++ playwright.config.js
@@ -30,9 +30,15 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    baseURL: 'https://google.com',
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure'
   },
-
+  // @ts-ignore
+  reporter: [
+    ['html'],
+    ['line']
+  ],
   /* Configure projects for major browsers */
   projects: [
     {
