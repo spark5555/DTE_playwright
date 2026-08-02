4b222ddbdd8436311b9443486e42d9c848c71d07
spark5555 <pahalageumesh@gmail.com>
2026-08-02 18:47:37 +0000

    Add toHaveTitle

--- tests/my_first_file.spec.js
+++ tests/my_first_file.spec.js
@@ -3,5 +3,6 @@ const {test, expect} = require('@playwright/test')
 test('My first test', async ({page}) => {
 
     await page.goto('https://google.com')
-
+    //expect(await page.title()).toBe('Google')
+    await expect(page).toHaveTitle('Google')
 })
